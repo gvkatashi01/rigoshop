2fd1ebbea496a3211117d75e4f3c3b75b8a2c444
Đàm Quang Minh <minhdq@vietnamlab.vn>
2019-06-21 15:26:44 +0700

    complete load data from database

--- BACKEND/rigo_product_manager_api/routes/index.js
+++ BACKEND/rigo_product_manager_api/routes/index.js
@@ -12,17 +12,33 @@ var connection = mysql.createConnection({
  
 /* GET home page. */
 router.get('/', function(req, res, next) {  
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
 });
 
 router.get('/products', function(req, res, next){
-  connection.connect();
 
-  connection.query('SELECT * FROM oasis_shop.products LIMIT 10;', function (error, results, fields) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+  //connection.connect();
+
+  var query = 'SELECT product_name, recommend_price , main_image FROM oasis_shop.products LIMIT 10;';
+  connection.query( query , function (error, results, fields) {
   if (error) throw error;
     console.log('The solution is: ', results[0]);
+    res.send(results)
   });
-  connection.end();
+  //connection.end();
 });
 
 module.exports = router;
